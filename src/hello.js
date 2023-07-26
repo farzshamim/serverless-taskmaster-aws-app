1ac11d726e538867ebf0917b3c965346551ad2d7
FARHAN SHAMIM <134058772+farzshamim@users.noreply.github.com>
2023-07-26 21:29:09 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Batch 3 serverless!",
+        message: "going serverless!",
       },
       null,
       2
